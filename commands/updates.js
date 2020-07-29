bc8c9d051dc8eba8ac90ccdb3e5677da7ac7582a
Redgames0 <51034701+Redgames0@users.noreply.github.com>
2020-07-29 18:53:07 +0100

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -6,20 +6,21 @@ module.exports = {
     cooldown: 1,
     guildOnly: true,
     args: true,
-    usage: '<Embed> <Text>', //Add Embed True / False Option? If no args 0 use embed |
+    usage: '<Embed | True / False> <Text>', //Add Embed True / False Option? If no args 0 use embed |
 	execute(message, args) {
 
         if(args[0] === 'True'){
 
             const update = new Discord.MessageEmbed()
                 .setColor('#5dfa39')
-                .setTitle('Gemcraft Survival | 📝 Update')
+                .setTitle('📖 Update • Gemcraft Survival')
                 // .setThumbnail(`https://i.imgur.com/jkx7wP0.png`)
                 .setTimestamp()
                 .setDescription('```diff\n' + args.slice(1).join(" ") + '```')
                 .setFooter(`Sent by: ${message.author.username} | play.gemcraftmc.net`)
 
             message.channel.send(update);
+            message.channel.delete;
         }if(args[0] === 'False'){
             message.channel.send(args.slice(1).join(" "))
         }else
