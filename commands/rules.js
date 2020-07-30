9e77b73785a49af74f51d9a7d3af40af11234f71
Redgames0 <51034701+Redgames0@users.noreply.github.com>
2020-07-30 21:40:12 +0100

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -10,7 +10,7 @@ module.exports = {
 
         const update = new Discord.MessageEmbed()
             .setColor('#32fc36')
-            .setTitle('Gemcraft Survival • Discord Rules')
+            .setTitle('Welcome to Gemcraft Survival • Discord Rules')
             // .setThumbnail(`https://i.imgur.com/jkx7wP0.png`)
             .setDescription(args.slice(0).join(" "))
             .setFooter(`Join Our Server • play.gemcraftmc.net`)
