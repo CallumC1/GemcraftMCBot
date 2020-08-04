8c1e4d894b556a1cc86532868d4e4be790f93d5b
Redgames0 <51034701+Redgames0@users.noreply.github.com>
2020-08-04 01:06:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -35,9 +35,9 @@ client.on('guildMemberAdd', async member => {
 	const memberJoin = new Discord.MessageEmbed()
 	.setColor('#21ed69')
 	.setTitle(`Welcome to Gemcraft Survival.`)
-	.setThumbnail(member.displayAvatarURL)
+	.setThumbnail(member.user.displayAvatarURL)
 	.setTimestamp()
-	.setDescription(`Welcome, ${member}\n**Server IP ▸** play.gemcraftmc.net \n**Website ▸** Coming Soon! \n**Store ▸**store.gemcraftmc.net\n `)
+	.setDescription(`Welcome, ${member}\n \u200b **Server IP ▸** play.gemcraftmc.net \n**Website ▸** Coming Soon! \n**Store ▸**store.gemcraftmc.net\n `)
 	.setFooter(`Click the ✅ in #📜general-rules to verify!`)
 
 channel.send(memberJoin)
