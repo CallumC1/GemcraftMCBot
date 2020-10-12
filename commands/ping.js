aaf9fe921c05b1ef8517506120e13ff108b16479
Redgames0 <51034701+Redgames0@users.noreply.github.com>
2020-10-12 20:48:12 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'Pong!',
 	cooldown: 1,
 	execute(message, args) {
-		const ping = Date.now() - message.createdTimestap;
-		message.channel.send(':ping_pong: Pong!' + `${Date.now() - message.createdTimestap}` + "ms");
+		var pingmsg = await message.channel.send("Pinging Server...")
+		message.channel.send(':ping_pong: Pong!' + `${pingmsg.Date.now() - message.createdTimestamp}` + "ms");
 	},
 };
