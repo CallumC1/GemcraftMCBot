93da1da5643f9e3a79370dc2c4137f911fbed22f
Redgames0 <51034701+Redgames0@users.noreply.github.com>
2020-10-19 18:45:49 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,11 +6,11 @@ module.exports = {
 	description: 'Pong! Get the bots ping.',
 	cooldown: 1,
 	execute(message, args) {
-		message.channel.send(`🏓 Latency is ${Date.now() - message.createdTimestamp}ms.`);
+		// message.channel.send(`🏓 Latency is ${Date.now() - message.createdTimestamp}ms.`);
 
 		const botping = new Discord.MessageEmbed()
 			.setColor('#037bfc')
-			.setTitle('Bot Latency')
+			.setTitle('🏓 Bot Latency')
 			.setDescription(`Latency: ${Date.now() - message.createdTimestamp}ms`)
 
 		message.channel.send(botping)
