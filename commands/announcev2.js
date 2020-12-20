13d5db5f1a0dfa132442371d77b41de0dfc26bd8
Redgames0 <51034701+Redgames0@users.noreply.github.com>
2020-12-20 22:09:27 +0000

    Create announcev2.js

---
+++ commands/announcev2.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'annoucement2',
+    alias: ['an'],
+    description: 'Testing',
+    role: 'Admin',
+    guildOnly: true,
+	cooldown: 1,
+	execute(message, args) {
+
+        //1. Send Await Message Asking If it should be an embed or not.
+        //2. Print Message saying what they picked & delete thebots message & users message.
+        //3. Send Another Await Message which contains what they would like to broadcast.
+        //4. Delete Bot & user messages.
+        //5. Send a Preview in the channel they are in.
+        //6. Send Await Embed asking what channel they would like to send it in & to confirm if they want to send it.
+        //7. Get channel, Check it exists, Send the Message / Embed.
+	},
+};
